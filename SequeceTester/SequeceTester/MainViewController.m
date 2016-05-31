652c72c3dbaf84d0a1f1ec1ded4b361e7c07ae4b
Elenore-iMac <chsy0823@gmail.com>
2016-05-31 17:20:55 +0900

    send data ok

--- SequeceTester/SequeceTester/MainViewController.m
+++ SequeceTester/SequeceTester/MainViewController.m
@@ -207,7 +207,8 @@
     else if(volume <0)
         volume = 0;
     
-    NSLog(@"volume = %f",volume);
+    currentVolume = volume;
+    NSLog(@"volume = %f",currentVolume);
     [[MPMusicPlayerController applicationMusicPlayer]setVolume:currentVolume];
     //[audioPlayer setVolume:volume];
 }
@@ -321,7 +322,7 @@
 - (void)setIP:(NSString*)ip Port:(NSString*)port {
     
     [networkController setServerWithIP:ip Port:[port intValue]];
-    [networkController sendCommand:002 Data:@"test"];
+    //[networkController sendCommand:002 Data:@"test"];
 }
 
 #pragma mark Bluetooth delegate
